1a66b371a92eeafbea5271a785c07e12d0f5e8b6
JeyTix <tixi4615@gmail.com>
2024-09-25 11:40:23 -0500

    Primera Version

---
+++ src/App.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        ArrayList lista= new ArrayList<>();
+        lista.add(30);
+        lista.add(12);
+        lista.add(7);
+        lista.add(5);
+
+        System.out.println("Lista: "+lista);
+
+    }
+}
